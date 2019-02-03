4aeb2646303f3327ca86840789d0b86d3f7e8ec1
nestofsaws <zimorowicz@gmail.com>
2019-02-03 00:53:25 -0600

    set the jumbotron layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Bootstrap core CSS -->
 	<link href="{{ asset ('css/app.css') }}" rel="stylesheet" type="text/css"/>
-	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="{{ asset ('css/styles.css') }}" rel="stylesheet">
@@ -23,16 +23,18 @@
       <div class="collapse bg-dark" id="navbarHeader">
         <div class="container">
           <div class="row">
-            <div class="col-sm-8 col-md-7 py-4">
-              <h4 class="text-white">About</h4>
-              <p class="text-muted">3DFS designs and manufactures the most efficient way to deliver electricity. Embedding Real-Time computing into power networks for protection stability and security.</p>
+            <div class="col-sm-8 col-md-7 py-2">
+
+              <p class="text-white display-4 font-3dfs"><span class="red-3dfs bg-dark">3</span><span class="white-3dfs">DFS</span><span class="dark-3dfs">&nbsp;</span></p>
+
+              <p class="text-white">3DFS designs and manufactures the most efficient way to deliver electricity. Embedding Real-Time computing into power networks for protection stability and security.</p>
             </div>
             <div class="col-sm-4 offset-md-1 py-4">
-              <h4 class="text-white">Contact</h4>
+              <p class="text-white size-20">Contact</p>
               <ul class="list-unstyled">
                 <li><a href="#" class="text-white">Follow on Twitter</a></li>
                 <li><a href="#" class="text-white">Like on Facebook</a></li>
-                <li><a href="#" class="text-white">Email me</a></li>
+                <li><a href="#" class="text-white">Email us</a></li>
               </ul>
             </div>
           </div>
@@ -49,7 +51,7 @@
               <ul class="list-unstyled">
                 <li><a href="#" class="text-white">Follow on Twitter</a></li>
                 <li><a href="#" class="text-white">Like on Facebook</a></li>
-                <li><a href="#" class="text-white">Email me</a></li>
+                <li><a href="#" class="text-white">Email us</a></li>
               </ul>
             </div>
           </div>
@@ -57,18 +59,15 @@
       </div>
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
-          <a href="{{ route('home') }}" class="navbar-brand d-flex align-items-center">
+         <!-- <a href="{{ route('home') }}" class="navbar-brand d-flex align-items-center">
           	<img src="{{ asset ('png/3dfs-white-logo.png') }}" height="50">
-          </a>
-        <ul class="mr-auto list-inline mt-3">
+          </a> -->
+        <ul class="ml-auto list-inline mt-3">
           <li class="nav-item active list-inline-item">
-            <a class="nav-link text-success" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link text-success" href="#">Innovation <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item list-inline-item">
-            <a class="nav-link text-success" href="#">Link</a>
-          </li>
-          <li class="nav-item list-inline-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
+            <a class="nav-link text-success" href="#">Products</a>
           </li>
         </ul>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
@@ -80,43 +79,61 @@
 
     <main role="main">
 
-      <section class="jumbotron bg-dark rounded-0">
+      <section class="jumbotron jumbofade rounded-0 pt-2">
         <div class="container">
-        <div class="row">
-        <div class="col-md-6">
-          <h1 class="jumbotron-heading text-white display-3 roboto"><span class="red-3dfs overline">3</span><span class="overline">DFS</span></h1>
-          <h2 class="text-white">PROTECTING POWER NETWORKS</h2>
-          <h3 class="text-muted text-light">WITH CLEAN ELECTRICITY THAT:</h3>
-          <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-			  <div class="carousel-inner">
-				<div class="carousel-item active">
-					<h3 class="text-success">Reduces energy consumption</h3>    
-				</div>
-				<div class="carousel-item">
-					<h3 class="text-success">Improves power network stability</h3>    
-				</div>
-				<div class="carousel-item">
-					<h3 class="text-success">Enhances asset performance</h3>    
-				</div>
-				<div class="carousel-item">
-					<h3 class="text-success">Eliminates utility penalties</h3>    
-				</div>
-			  </div>
-			</div>
-          <!-- <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p> -->
-          <p class="mt-5">
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-          </p>
-          </div>
-        <div class="col-md-6">
-		<img class="img-fluid img-thumbnail" src="{{ asset ('jpg/vector-Q2.jpg') }}"/>
-		</div>          
-          </div>
+  
+			<div class="col-md-12 d-flex flex-wrap">
+			<div class="col-md-6 d-flex flex-wrap">
+			 <div class="col-12"> 
+			  <h1 class="text-white display-3 font-3dfs"><span class="red-3dfs bg-dark">3</span><span class="white-3dfs">DFS</span><span class="gray-3dfs">&nbsp;</span></h1>
+			  <h2 class="text-dark"><strong>PROTECTING POWER NETWORKS</strong></h2>
+			  <h3 class="text-light">WITH CLEAN ELECTRICITY THAT:</h3>
+					<div class="row">
+						<div class="col-12">
+
+						  <div class="carousel slide" data-ride="carousel">
+							  <div class="carousel-inner">
+								<div class="carousel-item active">
+									<h3 class="text-success">Reduces energy consumption</h3>    
+								</div>
+								<div class="carousel-item">
+									<h3 class="text-success">Improves power network stability</h3>    
+								</div>
+								<div class="carousel-item">
+									<h3 class="text-success">Enhances asset performance</h3>    
+								</div>
+								<div class="carousel-item">
+									<h3 class="text-success">Eliminates utility penalties</h3>    
+								</div>
+							  </div>
+							</div>
+							<a href="#" class="btn red-3dfs-button mr-3 my-2">Learn More</a>
+							<a href="#" class="btn btn-light my-3">Contact Us</a>
+						  </div>
+					  </div>
+			 </div>
+			 <div class="col-md-6 d-md-none">
+				<img class="img-fluid" src="{{ asset ('jpg/vector-Q2.jpg') }}"/>
+		     </div> 
+			 <div class="col-12">
+			  <h3 class="text-white text-center mt-5">Fast, Easy Installation. Done.</h3>
+			  <h4>Non-Invasive Installation,</h4>
+			  <h4>Maintenance Free Operation</h4>
+			  <p>The <a href="#" class="text-success">VectorQ Series power controller</a> is quickly and non-invasively installed into power networks. It can be done in <strong>20 minutes</strong> without disrupting power and does not require any maintenance.</p>
+			  <h4 class="mt-4">Instantly Clean Electricity</h4>
+			  <h4>and Secure Power Network</h4>
+			  <p>The VectorQ Series synchronizes the electricity for all of the loads in the panel for maximum energy efficiency, asset performance and protection. It also establishes a secure, autonomous power network and provides data and analytics on every load.</p>
+			 </div>	
+			</div>		 
+			 <div class="col-md-6 d-none d-sm-block">
+				<img class="img-fluid" src="{{ asset ('jpg/vector-Q2.jpg') }}"/>
+		     </div> 
+		   </div> 
+
+		   
+       
         </div>
       </section>
-
-
       <div class="album py-5 bg-light">
         <div class="container">
 
