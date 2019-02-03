c2a5643824391aac63b7f02bd0a711e3b8cd3186
nestofsaws <zimorowicz@gmail.com>
2019-02-03 13:24:15 -0600

    nav edits and card deck in body

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,6 +10,7 @@
     <!-- Bootstrap core CSS -->
 	<link href="{{ asset ('css/app.css') }}" rel="stylesheet" type="text/css"/>
 	<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+	<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'>
     <!-- Custom styles for this template -->
     <link href="{{ asset ('css/styles.css') }}" rel="stylesheet">
   </head>  
@@ -56,200 +57,107 @@
   
   <section class="jumbotron jumbofade rounded-0 pt-2">
      
-  <div class="container">
+
 			<div class="col-md-12 d-flex flex-wrap">
 			<div class="col-md-6 d-flex flex-wrap">
 			 <div class="col-12"> 
 			  <h1 class="text-white display-3 font-3dfs"><span class="red-3dfs bg-dark">3</span><span class="white-3dfs">DFS</span><span class="gray-3dfs">&nbsp;</span></h1>
-			  <h2 class="text-dark"><strong>PROTECTING POWER NETWORKS</strong></h2>
+			  <h2><strong>PROTECTING POWER NETWORKS</strong></h2>
 			  <h3 class="text-light">WITH CLEAN ELECTRICITY THAT:</h3>
 					<div class="row">
 						<div class="col-12">
-
 						  <div class="carousel slide" data-ride="carousel">
 							  <div class="carousel-inner">
 								<div class="carousel-item active">
-									<h3 class="text-success">Reduces energy consumption</h3>    
+									<h3 class="text-success"><strong>Reduces energy consumption</strong></h3>    
 								</div>
 								<div class="carousel-item">
-									<h3 class="text-success">Improves power network stability</h3>    
+									<h3 class="text-success"><strong>Improves power network stability</strong></h3>    
 								</div>
 								<div class="carousel-item">
-									<h3 class="text-success">Enhances asset performance</h3>    
+									<h3 class="text-success"><strong>Enhances asset performance</strong></h3>    
 								</div>
 								<div class="carousel-item">
-									<h3 class="text-success">Eliminates utility penalties</h3>    
+									<h3 class="text-success"><strong>Eliminates utility penalties</strong></h3>    
 								</div>
 							  </div>
 							</div>
-							<a href="#" class="btn red-3dfs-button mr-3 my-2">Learn More</a>
+							<a href="#" class="btn red-3dfs-button mr-3 my-2"><strong>Learn More</strong></a>
 							<a href="#" class="btn btn-light my-3">Contact Us</a>
 						  </div>
 					  </div>
 			 </div>
-			 <div class="col-md-6 d-md-none">
+			 <div class="col-md-6 d-block d-md-none">
 				<img class="img-fluid" src="{{ asset ('jpg/vector-Q2.jpg') }}"/>
 		     </div> 
 			 <div class="col-12">
-			  <h3 class="text-white text-center mt-5">Fast, Easy Installation. Done.</h3>
-			  <h4>Non-Invasive Installation,</h4>
-			  <h4>Maintenance Free Operation</h4>
-			  <p>The <a href="#" class="text-success">VectorQ Series power controller</a> is quickly and non-invasively installed into power networks. It can be done in <strong>20 minutes</strong> without disrupting power and does not require any maintenance.</p>
-			  <h4 class="mt-4">Instantly Clean Electricity</h4>
-			  <h4>and Secure Power Network</h4>
+			  <h3 class="text-center mt-4"><strong>Fast, Easy Installation: Done.</strong></h3>
+			  <div class="card bg-light">
+			  <div class="card-body">
+			  <h4>Non-Invasive, Maintenance Free Operation</h4>
+			  <p>The <a href="#" class="text-success"><strong>VectorQ Series power controller</strong></a> is quickly and non-invasively installed into power networks. It can be done in <strong>20 minutes</strong> without disrupting power and does not require any maintenance.</p>
+			  </div>
+			  </div>
+			  <div class="card bg-light mt-4">
+			  <div class="card-body">
+			  <h4>Instantly Clean Electricity. Secure Power Network</h4>
 			  <p>The VectorQ Series synchronizes the electricity for all of the loads in the panel for maximum energy efficiency, asset performance and protection. It also establishes a secure, autonomous power network and provides data and analytics on every load.</p>
 			 </div>	
+			 </div>	
+			 </div>	
+			 
 			</div>		 
-			 <div class="col-md-6 d-none d-sm-block">
+			 <div class="col-md-6 d-none d-md-block">
 				<img class="img-fluid" src="{{ asset ('jpg/vector-Q2.jpg') }}"/>
 		     </div> 
 		   </div> 
-	</div> 
+	
       </section>
-      <div class="album py-5 bg-light">
+      <div class="py-5 bg-light">
         <div class="container">
+        
+<div class="px-3 py-3 pb-md-4 mx-auto text-center">
+  <h3 class="display-4">What Are The Benefits Of Clean Electricity?</h3>
+  <p class="lead">With a VectorQ Series power controller installed, every load in that panel will:</p>
+</div>
+          <div class="card-deck">
+              <div class="card mb-4 box-shadow">
+				<i class='fas fa-leaf text-success text-center mt-2' style='font-size:48px;'></i>				
+				<h4 class="text-center mt-2">Save Energy</h4>
+                <div class="card-body">
+					<p>10-15% kWh reduction for fans, electronics, lighting loads, power supplies, etc.</p>    
+					<p>20-25% kWh reduction for compressors, pumps, and other similar induction motor based loads</p>    
+                </div>
+             
+            </div>
+              <div class="card mb-4 box-shadow">
+                	<i class='fas fa-thermometer-quarter text-success text-center mt-2' style='font-size:48px;'></i>				
+				<h4 class="text-center mt-2">Operate at Cooler Temperatures</h4>
+                <div class="card-body">
+					<p>Power supplies, wires, motors, pumps, etc. will experience a ~20 degree temperature drop as a result of preventing energy waste.</p>    
+                </div>
+              </div>
 
-          <div class="row">
-            <div class="col-md-4">
               <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                	<i class='fas fa-charging-station text-success text-center mt-2' style='font-size:48px;'></i>				
+				<h4 class="text-center mt-2">Fail Less Frequently</h4>
                 <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
+					<p>Loads operate stress free and never experience the power fluctuations and surges that exist in uncontrolled power networks.</p>    
                 </div>
               </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <div class="col-md-4">
               <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                	<i class='fas fa-sync text-success text-center mt-2' style='font-size:48px;'></i>				
+				<h4 class="text-center mt-2">Function Consistently</h4>
                 <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
+					<p>Motors operate at tag values with optimum efficiency.</p>
+					<p>Data transfer with minimal errors or re-transmission.</p>
+					<p>In series generators interoperate seamlessly.</p>    
+                </div> 
               </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        
+            
+            
 
     </div>
 
