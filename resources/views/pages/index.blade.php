021b42359492e2ef28d0f7d79c87bc3e9a52b69b
nestofsaws <zimorowicz@gmail.com>
2019-02-03 22:36:41 -0600

    indents to remove hanging div

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -14,157 +14,142 @@
     <!-- Custom styles for this template -->
     <link href="{{ asset ('css/styles.css') }}" rel="stylesheet">
   </head>  
-  <body>   
+  <body>  
 	<nav class="navbar bg-dark navbar-dark sticky-top">
-	  <a class="navbar-brand ml-2" href="#" id="navlogo"><p class="text-white display-4 font-3dfs"><span class="red-3dfs bg-dark">3</span><span class="white-3dfs">DFS</span><span class="dark-3dfs">&nbsp;</span></p></a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#toptop">
-		<span class="navbar-toggler-icon"></span>
-	  </button> 
-  
-  	 
-  	 <div class="bg-dark collapse navbar-collapse" id="toptop">
-	  <div class="row">
-		<div class="col-md-6 pl-3">
-		  <p class="text-white">3DFS designs and manufactures the most efficient way to deliver electricity. Embedding Real-Time computing into power networks for protection stability and security.</p>
-		</div>
-		<div class="col-md-6 pl-3">
-	
-		  <div class="row">
-		   <div class="col-6 pl-3">
-	
-		  <p class="text-white size-20">Contact</p>
-		  <ul class="list-unstyled">
-			<li><a href="#" class="text-white">Follow on Twitter</a></li>
-			<li><a href="#" class="text-white">Like on Facebook</a></li>
-			<li><a href="#" class="text-white">Email us</a></li>
-		  </ul>	
-		  </div>
-		  
-		    <div class="col-6 pl-3">		
-		  <p class="text-white size-20">Contact</p>
-		  <ul class="list-unstyled">
-			<li><a href="#" class="text-white">Follow on Twitter</a></li>
-			<li><a href="#" class="text-white">Like on Facebook</a></li>
-			<li><a href="#" class="text-white">Email us</a></li>
-		  </ul>	
-		    </div>
-		    </div>
-		    
-		    
-		</div>
-	  </div>      
-    </nav>
-  
-  <section class="jumbotron jumbofade rounded-0 pt-2">
-     
-
-			<div class="col-md-12 d-flex flex-wrap">
+		<a class="navbar-brand ml-2" href="#" id="navlogo"><p class="text-white display-4 font-3dfs"><span class="red-3dfs bg-dark">3</span><span class="white-3dfs">DFS</span><span class="dark-3dfs">&nbsp;</span></p></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#toptop">
+			<span class="navbar-toggler-icon"></span>
+		</button>   	 
+		<div class="bg-dark collapse navbar-collapse" id="toptop">
+			<div class="row">
+				<div class="col-md-6 pl-3">
+					<p class="text-white">3DFS designs and manufactures the most efficient way to deliver electricity. Embedding Real-Time computing into power networks for protection stability and security.</p>
+				</div>
+				<div class="col-md-6 pl-3">	
+					<div class="row">
+						<div class="col-6 pl-3">	
+							<p class="text-white size-20">Contact</p>
+							<ul class="list-unstyled">
+								<li><a href="#" class="text-white">Follow on Twitter</a></li>
+								<li><a href="#" class="text-white">Like on Facebook</a></li>
+								<li><a href="#" class="text-white">Email us</a></li>
+							</ul>	
+						</div>
+						<div class="col-6 pl-3">		
+							<p class="text-white size-20">Contact</p>
+							<ul class="list-unstyled">
+								<li><a href="#" class="text-white">Follow on Twitter</a></li>
+								<li><a href="#" class="text-white">Like on Facebook</a></li>
+								<li><a href="#" class="text-white">Email us</a></li>
+							</ul>	
+						</div>
+					</div>
+				</div>
+			</div>      
+		</div>      
+	</nav>
+	<section class="jumbotron jumbofade rounded-0 pt-2">
+		<div class="col-md-12 d-flex flex-wrap">
 			<div class="col-md-6 d-flex flex-wrap">
-			 <div class="col-12"> 
-			  <h1 class="text-white display-3 font-3dfs"><span class="red-3dfs bg-dark">3</span><span class="white-3dfs">DFS</span><span class="gray-3dfs">&nbsp;</span></h1>
-			  <h2><strong>PROTECTING POWER NETWORKS</strong></h2>
-			  <h3 class="text-light">WITH CLEAN ELECTRICITY THAT:</h3>
+				<div class="col-12"> 
+					<h1 class="text-white display-3 font-3dfs"><span class="red-3dfs bg-dark">3</span><span class="white-3dfs">DFS</span><span class="gray-3dfs">&nbsp;</span></h1>
+					<h2><strong>PROTECTING POWER NETWORKS</strong></h2>
+					<h3 class="text-light">WITH CLEAN ELECTRICITY THAT:</h3>
 					<div class="row">
 						<div class="col-12">
-						  <div class="carousel slide" data-ride="carousel">
-							  <div class="carousel-inner">
-								<div class="carousel-item active">
-									<h3 class="text-success"><strong>Reduces energy consumption</strong></h3>    
+							<div class="carousel slide" data-ride="carousel">
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<h3 class="text-success"><strong>Reduces energy consumption</strong></h3>    
+									</div>
+									<div class="carousel-item">
+										<h3 class="text-success"><strong>Improves power network stability</strong></h3>    
+									</div>
+									<div class="carousel-item">
+										<h3 class="text-success"><strong>Enhances asset performance</strong></h3>    
+									</div>
+									<div class="carousel-item">
+										<h3 class="text-success"><strong>Eliminates utility penalties</strong></h3>    
+									</div>
 								</div>
-								<div class="carousel-item">
-									<h3 class="text-success"><strong>Improves power network stability</strong></h3>    
-								</div>
-								<div class="carousel-item">
-									<h3 class="text-success"><strong>Enhances asset performance</strong></h3>    
-								</div>
-								<div class="carousel-item">
-									<h3 class="text-success"><strong>Eliminates utility penalties</strong></h3>    
-								</div>
-							  </div>
 							</div>
 							<a href="#" class="btn red-3dfs-button mr-3 my-2"><strong>Learn More</strong></a>
 							<a href="#" class="btn btn-light my-3">Contact Us</a>
-						  </div>
-					  </div>
-			 </div>
-			 <div class="col-md-6 d-block d-md-none">
-				<img class="img-fluid" src="{{ asset ('jpg/vector-Q2.jpg') }}"/>
-		     </div> 
-			 <div class="col-12">
-			  <h3 class="text-center mt-4"><strong>Fast, Easy Installation: Done.</strong></h3>
-			  <div class="card bg-light">
-			  <div class="card-body">
-			  <h4>Non-Invasive, Maintenance Free Operation</h4>
-			  <p>The <a href="#" class="text-success"><strong>VectorQ Series power controller</strong></a> is quickly and non-invasively installed into power networks. It can be done in <strong>20 minutes</strong> without disrupting power and does not require any maintenance.</p>
-			  </div>
-			  </div>
-			  <div class="card bg-light mt-4">
-			  <div class="card-body">
-			  <h4>Instantly Clean Electricity. Secure Power Network</h4>
-			  <p>The <a href="#" class="text-success"><strong>VectorQ Series</strong></a> synchronizes the electricity for all of the loads in the panel for maximum energy efficiency, asset performance and protection. It also establishes a secure, autonomous power network and provides data and analytics on every load.</p>
-			 </div>	
-			 </div>	
-			 <div class="d-flex justify-content-center">
-			 	<a href="#" class="btn red-3dfs-button mr-3 mt-4"><strong>More Applications</strong></a>
-			 </div>	
-			 </div>	
-			 
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 d-block d-md-none">
+					<img class="img-fluid" src="{{ asset ('jpg/vector-Q2.jpg') }}"/>
+				</div> 
+				<div class="col-12">
+					<h3 class="text-center mt-4"><strong>Fast, Easy Installation: Done.</strong></h3>
+					<div class="card bg-light">
+						<div class="card-body">
+							<h4>Non-Invasive, Maintenance Free Operation</h4>
+							<p>The <a href="#" class="text-success"><strong>VectorQ Series power controller</strong></a> is quickly and non-invasively installed into power networks. It can be done in <strong>20 minutes</strong> without disrupting power and does not require any maintenance.</p>
+						</div>
+					</div>
+					<div class="card bg-light mt-4">
+						<div class="card-body">
+							<h4>Instantly Clean Electricity. Secure Power Network</h4>
+							<p>The <a href="#" class="text-success"><strong>VectorQ Series</strong></a> synchronizes the electricity for all of the loads in the panel for maximum energy efficiency, asset performance and protection. It also establishes a secure, autonomous power network and provides data and analytics on every load.</p>
+						</div>	
+					</div>	
+					<div class="d-flex justify-content-center">
+						<a href="#" class="btn red-3dfs-button mr-3 mt-4"><strong>More Applications</strong></a>
+					</div>	
+				</div>	
 			</div>		 
-			 <div class="col-md-6 d-none d-md-block">
+			<div class="col-md-6 d-none d-md-block">
 				<img class="img-fluid" src="{{ asset ('jpg/vector-Q2.jpg') }}"/>
-		     </div> 
-		   </div> 
+			</div> 
+		</div> 
+	</section>
 	
-      </section>
-      <div class="py-5 bg-light">
-        <div class="container">
-        
+	<div class="py-5 bg-light">
+		<div class="container">
 			<div class="px-3 py-3 pb-md-4 mx-auto text-center">
-			  <h3 class="display-4">What Are The Benefits Of Clean Electricity?</h3>
-			  <p class="lead">With a <a href="#" class="text-success"><strong>VectorQ Series Power Controller</strong></a> installed, every load in that panel will:</p>
-			</div>
+				<h3 class="display-4">What Are The Benefits Of Clean Electricity?</h3>
+				<p class="lead">With a <a href="#" class="text-success"><strong>VectorQ Series Power Controller</strong></a> installed, every load in that panel will:</p>
 		</div>
-
-          <div class="card-deck">
-              <div class="card mb-4 box-shadow">
+	</div>
+	<div class="container">
+		<div class="card-deck">
+			<div class="card mb-4 box-shadow">
 				<i class='fas fa-leaf text-success text-center mt-2' style='font-size:48px;'></i>				
 				<h4 class="text-center mt-2">Save Energy</h4>
-                <div class="card-body">
+				<div class="card-body">
 					<p>10-15% kWh reduction for fans, electronics, lighting loads, power supplies, etc.</p>    
 					<p>20-25% kWh reduction for compressors, pumps, and other similar induction motor based loads</p>    
-                </div>
-             
-            </div>
-              <div class="card mb-4 box-shadow">
-                	<i class='fas fa-thermometer-quarter text-success text-center mt-2' style='font-size:48px;'></i>				
+				</div>
+			</div>
+			<div class="card mb-4 box-shadow">
+				<i class='fas fa-thermometer-quarter text-success text-center mt-2' style='font-size:48px;'></i>				
 				<h4 class="text-center mt-2">Operate at Cooler Temperatures</h4>
-                <div class="card-body">
+				<div class="card-body">
 					<p>Power supplies, wires, motors, pumps, etc. will experience a ~20 degree temperature drop as a result of preventing energy waste.</p>    
-                </div>
-              </div>
-
-              <div class="card mb-4 box-shadow">
-                	<i class='fas fa-charging-station text-success text-center mt-2' style='font-size:48px;'></i>				
+				</div>
+			</div>
+			<div class="card mb-4 box-shadow">
+				<i class='fas fa-charging-station text-success text-center mt-2' style='font-size:48px;'></i>				
 				<h4 class="text-center mt-2">Fail Less Frequently</h4>
-                <div class="card-body">
+				<div class="card-body">
 					<p>Loads operate stress free and never experience the power fluctuations and surges that exist in uncontrolled power networks.</p>    
-                </div>
-              </div>
-
-              <div class="card mb-4 box-shadow">
-                	<i class='fas fa-sync text-success text-center mt-2' style='font-size:48px;'></i>				
+				</div>
+			</div>
+			<div class="card mb-4 box-shadow">
+				<i class='fas fa-sync text-success text-center mt-2' style='font-size:48px;'></i>				
 				<h4 class="text-center mt-2">Function Consistently</h4>
-                <div class="card-body">
+				<div class="card-body">
 					<p>-Motors operate at tag values with optimum efficiency.</p>
 					<p>-Data transfer with minimal errors or re-transmission.</p>
 					<p>-In series generators interoperate seamlessly.</p>    
-                </div> 
-              </div>
-        
-            
-    </div>
-    </div>
+				</div> 
+			</div>
+		</div>
+	</div>
     
+  <!--  
         <footer class="pt-4 my-md-5 pt-md-5 border-top bg-dark">
         <div class"container">
         <div class="row">
@@ -207,8 +192,8 @@
           </div>
         </div>
         </div>
-      </footer>
-    </div>  
+        </footer>
+-->
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
